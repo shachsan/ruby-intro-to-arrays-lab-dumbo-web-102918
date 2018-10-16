85ca13e2e9010b8683a4d0b971fa834a5e7d6056
SANJAY K SAI <shachsan@hotmail.com>
2018-10-16 02:52:20 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,11 +18,11 @@ def last_element(arr)
   arr[-1]
 end
 
-def first_country(arr)
+def first_element_with_array_methods(arr)
   arr.first
 end
 
-def last_country(arr)
+def last_element_with_array_methods(arr)
   arr.last
 end
 
