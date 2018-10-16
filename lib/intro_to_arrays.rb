cc5886ea881be93e3f028827d96c162ec08a0034
SANJAY K SAI <shachsan@hotmail.com>
2018-10-16 02:46:53 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,27 @@
+instantiate_new_array = []
+array_with_two_elements = [1,3]
+
+def my_first_element(arr)
+  arr[0]
+end 
+
+def my_third_element(arr)
+  arr[2]
+end
+
+def my_last_element(arr)
+  arr[-1]
+end 
+
+def first_country(arr)
+  arr.first
+end 
+
+def last_country(arr)
+  arr.last
+end
+
+def length_of_array(arr)
+  arr.length
+end 
+  
